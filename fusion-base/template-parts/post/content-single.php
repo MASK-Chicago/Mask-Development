4c5ca92e45caef057517cf8145a23ccde025dcf2
Qin, Boiar <boiar.qin@accenture.com>
2017-03-29 08:42:59 -0500

    modify single post template part

--- fusion-base/template-parts/post/content-single.php
+++ fusion-base/template-parts/post/content-single.php
@@ -10,7 +10,8 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('container'); ?>>
+	<h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 	<?php the_content(); ?>
 	<?php wp_link_pages(); ?>
 </article>
